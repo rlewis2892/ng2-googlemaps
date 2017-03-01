6ccb9a85bf46f3df794e305a90673f37bd6b728e
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-01 14:09:01 -0700

    updated all app files for google maps

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,19 @@
-import {NgModule} from "@angular/core";
+import {NgModule, ApplicationRef} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
+
+// for angular google maps
+import {CommonModule} from "@angular/common";
+import {AgmCoreModule} from "angular2-google-maps/core";
+
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, FormsModule, HttpModule, routing],
+	imports:      [BrowserModule, FormsModule, HttpModule, routing, CommonModule, AgmCoreModule.forRoot({apiKey: 'AIzaSyCQcLemUhUYrUpLF_G3EJ_AoFTN-YXtfp4'})],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [appRoutingProviders]
