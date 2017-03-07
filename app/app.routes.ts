1c9545d0e71226cae4178198720f9e56ff76ae80
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-07 11:37:43 -0700

    update routes with "/home" option, update 404 template

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -7,6 +7,7 @@ export const allAppComponents = [NotFoundComponent, HomeComponent, MapComponent]
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
+	{path: "home", component: HomeComponent},
 	{path: "**", component: NotFoundComponent}
 ];
 
--- public_html/templates/notfound.php
+++ public_html/templates/notfound.php
@@ -1,1 +1,4 @@
-<h1>404 Not Found</h1>
+<div class="container text-center">
+	<h1>404 Page Not Found</h1>
+	<a routerLink="/home" class="btn btn-lg btn-info">Go Home</a>
+</div>
